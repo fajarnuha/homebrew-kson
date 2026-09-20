becf32f030e85f4a879422b85ce361e6c4d022ba
Fajar Ulin Nuha <fajarunuha@gmail.com>
2026-09-20 09:10:51 +0700

    fix: use audited platform resources for binaries

--- Formula/kson.rb
+++ Formula/kson.rb
@@ -1,24 +1,29 @@
 class Kson < Formula
   desc "JSON toolkit with jq-compatible queries"
   homepage "https://github.com/fajarnuha/kson"
-  version "0.3.2"
+  url "https://github.com/fajarnuha/kson/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "974bcfa35bdd307ec369d171d9c98730a9a76063ab2ff8f5b53e5f79eda0faba"
   license "Apache-2.0"
 
   depends_on :macos
 
-  on_arm do
-    url "https://github.com/fajarnuha/kson/releases/download/v0.3.2/kson-macos-arm64.tar.gz"
-    sha256 "21d40f143f214bfbe766a0a1810a172b9fbf78b95254eed42445f4e735e5d548"
-  end
+  resource "binary" do
+    on_arm do
+      url "https://github.com/fajarnuha/kson/releases/download/v0.3.2/kson-macos-arm64.tar.gz"
+      sha256 "21d40f143f214bfbe766a0a1810a172b9fbf78b95254eed42445f4e735e5d548"
+    end
 
-  on_intel do
-    url "https://github.com/fajarnuha/kson/releases/download/v0.3.2/kson-macos-x64.tar.gz"
-    sha256 "5742504a821c3600f607d4d68b8680e04dd8d6afe4328c7d6d768cd33a36ffbd"
+    on_intel do
+      url "https://github.com/fajarnuha/kson/releases/download/v0.3.2/kson-macos-x64.tar.gz"
+      sha256 "5742504a821c3600f607d4d68b8680e04dd8d6afe4328c7d6d768cd33a36ffbd"
+    end
   end
 
   def install
-    bin.install "kson"
-    pkgshare.install "LICENSE"
+    resource("binary").stage do
+      bin.install "kson"
+      pkgshare.install "LICENSE"
+    end
   end
 
   test do
